ff47d563d5effc3286c1a9c89d028e957d6923c7
ianabreu <ianabreudev@gmail.com>
2023-05-30 04:13:29 -0300

    Iniciando projeto, instalando typescript e configurando servidor

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response, NextFunction } from "express";
+import { router } from "./routes";
+
+const PORT = 3333;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`servidor rodando na porta ${PORT}`);
+});
