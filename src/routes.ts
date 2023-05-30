227d201484b238042c2d7c74c38c1282f117c280
ianabreu <ianabreudev@gmail.com>
2023-05-30 04:41:32 -0300

    Tratando erros e instalando cors

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get("/teste", (req: Request, res: Response) => {
-  return res.json({ name: "Ian" });
+  return res.json({ name: "marcia" });
 });
 
 export { router };
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
+import cors from "cors";
+
 import { router } from "./routes";
 
 const PORT = 3333;
@@ -6,8 +9,23 @@ const PORT = 3333;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    //se for umas instacia do tipo Error
+    return res.status(400).json({
+      error: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal server error.",
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`servidor rodando na porta ${PORT}`);
