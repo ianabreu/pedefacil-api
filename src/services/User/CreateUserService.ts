18b8597b1f22f2de9488692b30c8bd1fa4e68230
ianabreu <ianabreudev@gmail.com>
2023-06-01 03:51:33 -0300

    cadastrando usuario no banco

--- src/services/User/CreateUserService.ts
+++ src/services/User/CreateUserService.ts
@@ -1,3 +1,5 @@
+import prismaClient from "../../prisma";
+
 interface UserRequest {
   name: string;
   email: string;
@@ -6,8 +8,31 @@ interface UserRequest {
 
 class CreateUserService {
   async execute({ name, email, password }: UserRequest) {
-    console.log(name);
-    return { ok: true };
+    //Verificar se enviou o email
+    if (!email) throw new Error("email incorrect");
+
+    //Verificar se o email já está cadastrado
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+    if (userAlreadyExists) throw new Error("user already exists");
+    // Cadastrar user
+    const user = await prismaClient.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: password,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
+    });
+
+    return user;
   }
 }
 export { CreateUserService };
