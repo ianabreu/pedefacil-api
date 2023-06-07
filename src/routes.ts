83461b76d19b58e85894a580783b76da0bcc7c40
ianabreu <ianabreudev@gmail.com>
2023-06-07 04:54:20 -0300

    função de listar produtos por categoria

--- src/routes.ts
+++ src/routes.ts
@@ -12,6 +12,7 @@ import { CreateCategoryController } from "./controllers/category/CreateCategoryC
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
 
 import { CreateProductController } from "./controllers/product/CreateProductController";
+import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
 
 const router = Router();
 const upload = multer(uploadConfig.upload("./tmp"));
@@ -20,6 +21,7 @@ const upload = multer(uploadConfig.upload("./tmp"));
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
 router.get("/me", isAuthenticated, new DetailUserController().handle);
+
 //-- ROTAS CATEGORY --
 router.post(
   "/category",
@@ -27,12 +29,19 @@ router.post(
   new CreateCategoryController().handle
 );
 router.get("/category", isAuthenticated, new ListCategoryController().handle);
+
 //-- ROTAS PRODUCT --
 router.post(
   "/product",
   isAuthenticated,
   upload.single("file"),
   new CreateProductController().handle
+);
+
+router.get(
+  "/category/product",
+  isAuthenticated,
+  new ListByCategoryController().handle
 );
 
 export { router };
