8ce46ad6fc07734d613ff34ba585a04ee4d4c2ce
ianabreu <ianabreudev@gmail.com>
2023-06-09 04:30:11 -0300

    Removendo item de uma Order - Pedido

--- src/routes.ts
+++ src/routes.ts
@@ -17,6 +17,7 @@ import { ListByCategoryController } from "./controllers/product/ListByCategoryCo
 import { CreateOrderController } from "./controllers/order/CreateOrderController";
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
 import { AddItemController } from "./controllers/order/AddItemController";
+import { RemoveItemController } from "./controllers/order/RemoveItemController";
 
 const router = Router();
 const upload = multer(uploadConfig.upload("./tmp"));
@@ -53,4 +54,9 @@ router.post("/order", isAuthenticated, new CreateOrderController().handle);
 router.delete("/order", isAuthenticated, new RemoveOrderController().handle);
 
 router.post("/order/add", isAuthenticated, new AddItemController().handle);
+router.delete(
+  "/order/remove",
+  isAuthenticated,
+  new RemoveItemController().handle
+);
 export { router };
